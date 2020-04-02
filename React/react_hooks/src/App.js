09e0772d053ccbb09545e26a6c3cd2cc843f9acc
rockshang <s2265681@163.com>
2020-04-02 22:25:31 +0800

    更新react hooks

--- React/react_hooks/src/App.js
+++ React/react_hooks/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
-import ReactHooks from "./components/ReactHooks"
+// import ReactHooks from "./components/ReactHooks"
 // import HookUseReducer from './components/UseReducer1'
 // import HookUseReducer from './components/UseReducer2'
+import Picture from './components/Picture'
 
 
 function App() {
   return (
     <div className="App">
-      <ReactHooks/>
+      <Picture/>
     </div>
   );
 }
