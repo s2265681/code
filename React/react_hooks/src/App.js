61c37d1eb89d6ef80974d2d5436c73eae114e5c3
rockshang <s2265681@163.com>
2020-04-04 02:04:53 +0800

    react hooks fix item list

--- React/react_hooks/src/App.js
+++ React/react_hooks/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
-// import ReactHooks from "./components/ReactHooks"
-// import HookUseReducer from './components/UseReducer1'
-// import HookUseReducer from './components/UseReducer2'
-import Picture from './components/Picture'
-
+import './App.css';
+import ReactHooks from "./components/ReactHooks";
+import Home from './pages/Home';
+import HookUseReducer1 from './components/UseReducer1';
+import HookUseReducer2 from './components/UseReducer2';
+import Picture from './components/Picture';
+import ItemIndex from './pages/Item';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Picture/>
-    </div>
+      <Router>
+        <div className="App">
+          <Link to="/" style={{margin:'0 10px'}}>Home</Link> 
+          <Link to="/ReactHooks" style={{margin:'0 10px'}}>ReactHook</Link>   
+          <Link to="/HookUseReducer1" style={{margin:'0 10px'}}>UseReducer1</Link> 
+          <Link to="/HookUseReducer2" style={{margin:'0 10px'}}>UseReducer2</Link> 
+          <Link to="/picture" style={{margin:'0 10px'}}>pictureSelect组件</Link>
+          <Link to="/itemIndex" style={{margin:'0 10px'}}>SeacherItem组件</Link>
+
+          <hr/>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/ReactHooks" component={ReactHooks}></Route>
+          <Route path="/HookUseReducer1" component={HookUseReducer1}></Route>
+          <Route path="/HookUseReducer2" component={HookUseReducer2}></Route>
+          <Route path="/picture" component={Picture}></Route>
+          <Route path="/itemIndex" component={ItemIndex}></Route>
+        </div>
+    </Router>
   );
 }
 
---
+++ React/react_hooks/src/pages/Home.js
@@ -0,0 +1,4 @@
+
+import React from 'react';
+const Home = ()=> <div>react hooks</div>
+export default Home;
