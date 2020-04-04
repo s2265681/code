560ef255ebf79601177742c2101648c9655ee707
rockshang <s2265681@163.com>
2020-04-04 10:36:18 +0800

    build

---
+++ React/react_hooks/build/precache-manifest.05e9988d2d2fa230460a838bf5c813bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008d0adf5cb04dbec6d0a5ca9c949a3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb225ca96d11640020a7",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "5aa583c8e9391713738e",
+    "url": "/static/css/main.1524e2a2.chunk.css"
+  },
+  {
+    "revision": "bb225ca96d11640020a7",
+    "url": "/static/js/2.68535e3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.68535e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa583c8e9391713738e",
+    "url": "/static/js/main.e965473b.chunk.js"
+  },
+  {
+    "revision": "8af4c01126615b37a406",
+    "url": "/static/js/runtime-main.25a0b8a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
