a91bcd5399f313162ae4407c7f7632a427150db8
rockshang <s2265681@163.com>
2020-04-04 10:59:00 +0800

    s

---
+++ React/react_hooks/build/precache-manifest.c6d494a08676804fc9566db2b0941a66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064d0570c447b09485df7f2be2311a29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb225ca96d11640020a7",
+    "url": "./static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "f9e61a96e34044324075",
+    "url": "./static/css/main.1524e2a2.chunk.css"
+  },
+  {
+    "revision": "bb225ca96d11640020a7",
+    "url": "./static/js/2.68535e3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.68535e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e61a96e34044324075",
+    "url": "./static/js/main.09426cab.chunk.js"
+  },
+  {
+    "revision": "d71b938af8a867bcba00",
+    "url": "./static/js/runtime-main.4a4dd43c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- React/react_hooks/build/service-worker.js
+++ React/react_hooks/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05e9988d2d2fa230460a838bf5c813bd.js"
+  "./precache-manifest.c6d494a08676804fc9566db2b0941a66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
