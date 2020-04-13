765d2db4659e0577002ebecd5896cdb7f94d5ee2
rockshang <s2265681@163.com>
2020-04-13 23:43:39 +0800

    table

--- vue/vueui/src/components/Table.vue
+++ vue/vueui/src/components/Table.vue
@@ -1,15 +1,115 @@
 <template>
-    <div>
-      Table
-    </div>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(col, index) in cloneColumns" :key="index">
+            <div
+              v-if="col.type == 'selection'"
+              class="selection"
+              @change="checkAll"
+            >
+              <input type="checkbox" checked="cloneColumns.some(e=>e.checked)"/>
+            </div>
+            <div v-else>
+              {{ col.title }}
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(row, index) in cloneData"
+          :key="index"
+          @mousemove="rowOver(index)"
+          @mouseout="rowOut(index)"
+          ref="row"
+        >
+          <td v-for="(col, index) in cloneColumns" :key="index">
+            <div
+              v-if="col.type == 'selection'"
+              class="selection"
+              @change="checkOne"
+            >
+              <input type="checkbox" checked="col.checked"/>
+            </div>
+            <div v-else>
+              {{ row[col.key] }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import cloneDeep from "lodash/cloneDeep";
+
+export default {
+  name: "table",
+  data() {
+    return {
+      cloneColumns: cloneDeep(this.columns),
+      cloneData: cloneDeep(this.data),
+    };
+  },
+  methods: {
+    checkAll() {},
+    checkOne() {},
+    rowOver(i) {
+      this.$refs.row[i].style.background = "#eee";
+    },
+    rowOut(i) {
+      this.$refs.row[i].style.background = "#fff";
+    },
+  },
+  mounted() {
+    console.log(this.cloneColumns, "//");
+    console.log(this.columns, "//");
+    this.cloneColumns.forEach(el=>{
+   
+      el.checked = false
+         console.log(el,'el')
+    })
+    console.log(this.cloneColumns, "2er/");
+  },
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.container {
+  width: 100%;
+}
+.container table {
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 1px solid #eee;
+  text-align: left;
+}
+.container table th {
+  background: #eee;
+}
+.container table td,
+.container table th {
+  border: 1px solid #eee;
+  height: 50px;
+  line-height: 50px;
+}
 
+.selection {
+  text-align: center;
+  vertical-align: middle;
+}
 </style>
--- vue/vueui/src/pages/Table.vue
+++ vue/vueui/src/pages/Table.vue
@@ -1,15 +1,71 @@
 <template>
-    <div>
-        表格
-    </div>
+  <div class="tatle">
+    <Table :columns="columns" :data="data"></Table>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import Table from '../components/Table'
+export default {
+        name:'',
+        components:{
+            Table
+        },
+        data(){
+            return{
+                columns: [
+                     {
+                        type: 'selection',
+                        width: 60,
+                        align: 'center'
+                    },
+                    {
+                        title: 'Name',
+                        key: 'name',
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age',
+                        sortable: true
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+   .tatle{
+       margin: 10px 20px;
+   }
 </style>
