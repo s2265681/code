46becd693e8ba15ecebded3c9edb20654e336cc0
rockshang <s2265681@163.com>
2020-05-31 12:38:18 +0800

    fix

--- React_Hook_UI/rockui/src/App.tsx
+++ React_Hook_UI/rockui/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Button, { ButtonSize, ButtonType } from "./components/Button/button";
 import Alert, { AlertType } from "./components/Alert/alert";
 import Menu from "./components/Menu/menu";
